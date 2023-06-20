95f83d0814d166d8a10879d09a4437acb952f09d
siparat1712 <akimov1712@yandex.ru>
2023-06-20 12:46:12 +0300

    2

--- app/src/main/java/ru/steelwave/unotes/presentation/InformationInRegistrationFragment.kt
+++ app/src/main/java/ru/steelwave/unotes/presentation/InformationInRegistrationFragment.kt
@@ -1,27 +1,24 @@
 package ru.steelwave.unotes.presentation
 
-import android.content.res.ColorStateList
-import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ru.steelwave.unotes.R
-import ru.steelwave.unotes.databinding.FragmentInformationBinding
+import androidx.fragment.app.Fragment
+import ru.steelwave.unotes.databinding.FragmentInformationInRegistrationBinding
 
 
-class InformationFragment : Fragment() {
+class InformationInRegistrationFragment : Fragment() {
 
-    private var _binding: FragmentInformationBinding? = null
-    private val binding: FragmentInformationBinding
-    get() = _binding ?: throw RuntimeException("FragmentInformationBinding == null")
+    private var _binding: FragmentInformationInRegistrationBinding? = null
+    private val binding: FragmentInformationInRegistrationBinding
+        get() = _binding ?: throw RuntimeException("FragmentInformationBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentInformationBinding.inflate(inflater)
+        _binding = FragmentInformationInRegistrationBinding.inflate(inflater)
         return binding.root
     }
 
@@ -29,6 +26,7 @@ class InformationFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         settingView()
     }
+
     private fun settingView() {
 
     }
