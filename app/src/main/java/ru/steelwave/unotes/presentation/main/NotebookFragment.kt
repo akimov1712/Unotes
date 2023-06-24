954216cd9f69e7a6cd69de773b79d006fa193d8c
siparat1712 <akimov1712@yandex.ru>
2023-06-24 09:54:36 +0300

    6

---
+++ app/src/main/java/ru/steelwave/unotes/presentation/main/NotebookFragment.kt
@@ -0,0 +1,30 @@
+package ru.steelwave.unotes.presentation.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.steelwave.unotes.R
+import ru.steelwave.unotes.databinding.FragmentNotebookBinding
+
+
+class NotebookFragment : Fragment() {
+
+    private var _binding: FragmentNotebookBinding? = null
+    private val binding: FragmentNotebookBinding
+    get() = _binding ?: throw RuntimeException("FragmentNotebookBinding == null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+       _binding = FragmentNotebookBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
