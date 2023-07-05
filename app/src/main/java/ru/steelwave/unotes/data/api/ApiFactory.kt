bbd0257e079719967249e52ab5f18bfe1dc93ec0
Siparat <maksimak1712@mail.ru>
2023-07-05 13:22:16 +0300

    3

--- app/src/main/java/ru/steelwave/unotes/data/api/ApiFactory.kt
+++ app/src/main/java/ru/steelwave/unotes/data/api/ApiFactory.kt
@@ -1,10 +1,15 @@
 package ru.steelwave.unotes.data.api
 
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 
 object ApiFactory {
 
+    private const val BASE_URL = ""
     private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
 }
